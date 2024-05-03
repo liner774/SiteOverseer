94ac2b75e4946b60d81a8911d6e125633a03b392
sawkhinkyawsoe <sawkhinkyawsoe223@gmail.com>
2024-05-03 14:03:10 +0630

    comment by khin

--- SiteOverseer/Program.cs
+++ SiteOverseer/Program.cs
@@ -17,7 +17,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-// commend by khin
+// comment by khin
 
 
 //comment by nyi lynn
