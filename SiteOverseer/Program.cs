9bac7119fa2decf157ba7edbff6fc7bf1cf7fa21
sawkhinkyawsoe <sawkhinkyawsoe223@gmail.com>
2024-05-06 21:12:45 +0630

    hello

--- SiteOverseer/Program.cs
+++ SiteOverseer/Program.cs
@@ -34,6 +34,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=TranTypeReasons}/{action=Index}/{id?}");
+    pattern: "{controller=Companies}/{action=Index}/{id?}");
 
 app.Run();
