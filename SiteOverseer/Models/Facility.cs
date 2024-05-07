44e88d46b7c529e562ad31ad47205df960217678
Ann Lynn <night7soul2@gmail.com>
2024-05-07 11:25:14 +0630

    facility by Lynn

--- SiteOverseer/Models/Facility.cs
+++ SiteOverseer/Models/Facility.cs
@@ -1,22 +1,31 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace SiteOverseer.Models
 {
     public class Facility
     {
         [Key]
+        [DisplayName("Facility ID")]
         public int FcilId { get; set; }
         [StringLength(50)]
+        [DisplayName("Facility Code")]
         public required string FcilCde {  get; set; }
+        [DisplayName("Facility Name")]
         public required string FcilNme { get; set; }
         public string? Address { get; set; }
         [StringLength(50)]
         public string? Township { get; set; }
+        [DisplayName("City")]
         public int CityId { get; set; }
+        [DisplayName("Facility Type")]
         public int FciltypId { get; set; }
+        [DisplayName("Contract Value")]
         public decimal? Contractval { get; set; }
-        public DateTime? ApproveDte { get; set; }
-        public DateTime? FcilstartDte { get; set; }
+        [DisplayName("Approved Date")]
+        public DateOnly? ApproveDte { get; set; }
+        [DisplayName("Facility Start Date")]
+        public DateOnly? FcilstartDte { get; set; }
         public short CmpyId { get; set; }
         public int UserId { get; set; }
         public DateTime RevdTetime { get; set; }
