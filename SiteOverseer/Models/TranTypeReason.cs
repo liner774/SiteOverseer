ae457f9e7c00a7da0df5a548a4ad2998cdee7c16
Ann Lynn <night7soul2@gmail.com>
2024-05-07 09:39:18 +0630

    TranReason by Lynn

--- SiteOverseer/Models/TranTypeReason.cs
+++ SiteOverseer/Models/TranTypeReason.cs
@@ -11,8 +11,11 @@ namespace SiteOverseer.Models
         [StringLength(50)]
         [DisplayName("Reason")]
         public required string TranReasonDesc { get; set; }
+        [DisplayName("Company")]
         public short CmpyId { get; set; }
+        [DisplayName("User")]
         public int UserId { get; set; }
+        [DisplayName("Revised Datetime")]
         public DateTime RevDtetime { get; set; }
     }
 }
