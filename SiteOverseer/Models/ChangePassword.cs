c48fc279a37973fe6b908d69f3da9b29aa791350
sawkhinkyawsoe <sawkhinkyawsoe223@gmail.com>
2024-05-27 15:45:29 +0630

    resetpassword by saw khin

--- SiteOverseer/Models/ChangePassword.cs
+++ SiteOverseer/Models/ChangePassword.cs
@@ -14,7 +14,21 @@ namespace SiteOverseer.Models
         [DisplayName("User Name")]
         public required string UserNme { get; set; }
         public string Password { get; set; } = string.Empty;
-        public string ConfirmPassword { get; set; } = string.Empty;
         public string NewPassword { get; set; } = string.Empty;
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+
+    public class ResetPasswordmodel
+    {
+        public int UserId { get; set; }
+        [StringLength(24)]
+        [DisplayName("User Code")]
+        public required string UserCde { get; set; }
+        [StringLength(100)]
+        [DisplayName("User Name")]
+        public required string UserNme { get; set; }
+       
+
+
     }
 }
