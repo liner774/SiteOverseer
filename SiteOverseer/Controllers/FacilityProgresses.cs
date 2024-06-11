c2568ef2ed17196998b01241d8e65f64a316d6f6
Ann Lynn <night7soul2@gmail.com>
2024-06-11 09:34:26 +0630

    fcilprogress_local_Lynn

---
+++ SiteOverseer/Controllers/FacilityProgresses.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SiteOverseer.Data;
+using SiteOverseer.Models;
+
+namespace SiteOverseer.Controllers
+{
+    public class FacilityProgresses : Controller
+    {
+        private readonly SiteDbContext _context;
+
+        public FacilityProgresses(SiteDbContext context)
+        {
+            _context = context;
+        }
+
+        #region // Main Methods //
+        public async Task<IActionResult> Index()
+        {
+            SetLayOutData();
+            var progList = await _context.PMS_Facilityprogress.ToListAsync();
+
+            foreach (var prog in progList)
+            {
+                prog.FcilId = _context.MS_Facilitytask.Where(gp => gp.FciltskId == prog.FcilTskId).Select(gp => gp.FcilId).FirstOrDefault();
+                prog.FcilNme = _context.MS_Facility.Where(gp => gp.FcilId == prog.FcilId).Select(gp => gp.FcilNme).FirstOrDefault();
+                prog.FcilCde = _context.MS_Facility.Where(gp => gp.FcilId == prog.FcilId).Select(gp => gp.FcilCde).FirstOrDefault();
+                prog.CntorId = _context.MS_Facilitytask.Where(gp => gp.FciltskId == prog.FcilTskId).Select(gp => gp.CntorId).FirstOrDefault();
+                prog.CntorNme = _context.MS_Contractor.Where(gp => gp.CntorId == prog.CntorId).Select(gp => gp.CntorNme).FirstOrDefault();
+                prog.WbsdId = _context.MS_Facilitytask.Where(gp => gp.FciltskId == prog.FcilTskId).Select(gp => gp.WbsdId).FirstOrDefault();
+                prog.WbsId = _context.MS_Wbsdetail.Where(gp => gp.WbsdId == prog.WbsdId).Select(gp => gp.WbsId).FirstOrDefault();
+                prog.WbsdCde = _context.MS_Wbsdetail.Where(gp => gp.WbsId == prog.WbsId).Select(gp => gp.WbsdCde).FirstOrDefault();
+                prog.WbsCde = _context.MS_Wbs.Where(gp => gp.WbsId == prog.WbsId).Select(gp => gp.WbsCde).FirstOrDefault();
+                prog.WorkstartDte = _context.MS_Facilitytask.Where(gp => gp.FciltskId == prog.FcilTskId).Select(gp => gp.WorkstartDte).FirstOrDefault();
+                prog.WorkendDte = _context.MS_Facilitytask.Where(gp => gp.FciltskId == prog.FcilTskId).Select(gp => gp.WorkendDte).FirstOrDefault();
+                prog.Budget = _context.MS_Facilitytask.Where(gp => gp.FciltskId == prog.FcilTskId).Select(gp => gp.Budget).FirstOrDefault();
+                
+            }
+
+            return View(progList);
+        }
+
+       
+        public async Task<IActionResult> Details(long? id)
+        {
+            SetLayOutData();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var facilityProgress = await _context.PMS_Facilityprogress
+                .FirstOrDefaultAsync(m => m.ProgId == id);
+            if (facilityProgress == null)
+            {
+                return NotFound();
+            }
+            
+            return View(facilityProgress);
+        }
+
+        
+        public IActionResult Create()
+        {
+            SetLayOutData();
+            
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProgId,FcilTskId,ProgPercent,SubmitDte,Longitude,Latitude,CmpyId,UserId,RevDteTime")] FacilityProgress facilityProgress)
+        {
+            if (ModelState.IsValid)
+            {
+                facilityProgress.RevDteTime = DateTime.Now;
+                facilityProgress.Longitude = 0;//default
+                facilityProgress.Latitude = 0;//default
+                facilityProgress.UserId = GetUserId();
+                facilityProgress.CmpyId = GetCmpyId();
+                _context.Add(facilityProgress);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(facilityProgress);
+        }
+
+       
+        public async Task<IActionResult> Edit(long? id)
+        {
+            SetLayOutData();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var facilityProgress = await _context.PMS_Facilityprogress.FindAsync(id);
+            if (facilityProgress == null)
+            {
+                return NotFound();
+            }
+            #region // facility task call //
+            facilityProgress.FcilId = _context.MS_Facilitytask.Where(gp => gp.FciltskId == facilityProgress.FcilTskId).Select(gp => gp.FcilId).FirstOrDefault();
+            facilityProgress.FcilNme = _context.MS_Facility.Where(gp => gp.FcilId == facilityProgress.FcilId).Select(gp => gp.FcilNme).FirstOrDefault();
+            facilityProgress.FcilCde = _context.MS_Facility.Where(gp => gp.FcilId == facilityProgress.FcilId).Select(gp => gp.FcilCde).FirstOrDefault();
+            facilityProgress.CntorId = _context.MS_Facilitytask.Where(gp => gp.FciltskId == facilityProgress.FcilTskId).Select(gp => gp.CntorId).FirstOrDefault();
+            facilityProgress.CntorNme = _context.MS_Contractor.Where(gp => gp.CntorId == facilityProgress.CntorId).Select(gp => gp.CntorNme).FirstOrDefault();
+            facilityProgress.WbsdId = _context.MS_Facilitytask.Where(gp => gp.FciltskId == facilityProgress.FcilTskId).Select(gp => gp.WbsdId).FirstOrDefault();
+            facilityProgress.WbsId = _context.MS_Wbsdetail.Where(gp => gp.WbsdId == facilityProgress.WbsdId).Select(gp => gp.WbsId).FirstOrDefault();
+            facilityProgress.WbsdCde = _context.MS_Wbsdetail.Where(gp => gp.WbsId == facilityProgress.WbsId).Select(gp => gp.WbsdCde).FirstOrDefault();
+            facilityProgress.WbsCde = _context.MS_Wbs.Where(gp => gp.WbsId == facilityProgress.WbsId).Select(gp => gp.WbsCde).FirstOrDefault();
+            facilityProgress.WorkstartDte = _context.MS_Facilitytask.Where(gp => gp.FciltskId == facilityProgress.FcilTskId).Select(gp => gp.WorkstartDte).FirstOrDefault();
+            facilityProgress.WorkendDte = _context.MS_Facilitytask.Where(gp => gp.FciltskId == facilityProgress.FcilTskId).Select(gp => gp.WorkendDte).FirstOrDefault();
+            facilityProgress.Budget = _context.MS_Facilitytask.Where(gp => gp.FciltskId == facilityProgress.FcilTskId).Select(gp => gp.Budget).FirstOrDefault();
+            facilityProgress.SelectionTyp = _context.MS_Facilitytask.Where(gp => gp.FciltskId == facilityProgress.FcilTskId).Select(gp => gp.SelectionTyp).FirstOrDefault();
+            facilityProgress.AwardedValue = _context.MS_Facilitytask.Where(gp => gp.FciltskId == facilityProgress.FcilTskId).Select(gp => gp.AwardedValue).FirstOrDefault();
+            facilityProgress.ProgpayId = _context.MS_Facilitytask.Where(gp => gp.FciltskId == facilityProgress.FcilTskId).Select(gp => gp.ProgpayId).FirstOrDefault();
+            facilityProgress.AllowSubmitExpense = _context.MS_Facilitytask.Where(gp => gp.FciltskId == facilityProgress.FcilTskId).Select(gp => gp.AllowSubmitExpense).FirstOrDefault();
+            facilityProgress.TaskCompleteFlg = _context.MS_Facilitytask.Where(gp => gp.FciltskId == facilityProgress.FcilTskId).Select(gp => gp.TaskCompleteFlg).FirstOrDefault();
+            facilityProgress.Remark = _context.MS_Facilitytask.Where(gp => gp.FciltskId == facilityProgress.FcilTskId).Select(gp => gp.Remark).FirstOrDefault();
+            facilityProgress.FC_Id = _context.MS_Facilitytask.Where(gp => gp.FciltskId == facilityProgress.FcilTskId).Select(gp => gp.CmpyId).FirstOrDefault();
+            facilityProgress.FU_Id = _context.MS_Facilitytask.Where(gp => gp.FciltskId == facilityProgress.FcilTskId).Select(gp => gp.UserId).FirstOrDefault();
+            facilityProgress.FCompany = _context.MS_Company.Where(gp => gp.CmpyId == facilityProgress.FC_Id).Select(gp => gp.CmpyNme).FirstOrDefault();
+            facilityProgress.FUser = _context.MS_User.Where(gp => gp.UserId == facilityProgress.FU_Id).Select(gp => gp.UserNme).FirstOrDefault();
+            #endregion
+
+            return View(facilityProgress);
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("ProgId,FcilTskId,SubmitDte,Longitude,Latitude,CmpyId,UserId,RevDteTime")] FacilityProgress facilityProgress)
+        {
+            if (id != facilityProgress.ProgId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    facilityProgress.RevDteTime = DateTime.Now;
+                    facilityProgress.Longitude = 0;//default
+                    facilityProgress.Latitude = 0;//default
+                    facilityProgress.UserId = GetUserId();
+                    facilityProgress.CmpyId = GetCmpyId();
+                    _context.Update(facilityProgress);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FacilityProgressExists(facilityProgress.ProgId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(facilityProgress);
+        }
+
+       
+        public async Task<IActionResult> Delete(long? id)
+        {
+            SetLayOutData();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var facilityProgress = await _context.PMS_Facilityprogress
+                .FirstOrDefaultAsync(m => m.ProgId == id);
+            if (facilityProgress == null)
+            {
+                return NotFound();
+            }
+
+            return View(facilityProgress);
+        }
+
+        
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var facilityProgress = await _context.PMS_Facilityprogress.FindAsync(id);
+            if (facilityProgress != null)
+            {
+                _context.PMS_Facilityprogress.Remove(facilityProgress);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FacilityProgressExists(long id)
+        {
+            return _context.PMS_Facilityprogress.Any(e => e.ProgId == id);
+        }
+        #endregion
+
+        #region // Global Methods (Important) //
+        protected short GetUserId()
+        {
+            var userCde = HttpContext.User.Claims.FirstOrDefault()?.Value;
+            var userId = (short)_context.MS_User
+                .Where(u => u.UserCde == userCde)
+                .Select(u => u.UserId)
+                .FirstOrDefault();
+
+            return userId;
+        }
+        protected short GetCmpyId()
+        {
+            var cmpyId = _context.MS_User
+                .Where(u => u.UserId == GetUserId())
+                .Select(u => u.CmpyId)
+                .FirstOrDefault();
+
+            return cmpyId;
+        }
+
+        protected void SetLayOutData()
+        {
+            var userCde = HttpContext.User.Claims.FirstOrDefault()?.Value;
+            if (userCde != null)
+            {
+                var userName = _context.MS_User
+                    .Where(u => u.UserCde == userCde)
+                    .Select(u => u.UserNme)
+                    .FirstOrDefault();
+
+                ViewData["Username"] = userName;
+            }
+        }
+        #endregion
+    }
+}
--- SiteOverseer/Models/FacilityProgress.cs
+++ SiteOverseer/Models/FacilityProgress.cs
@@ -1,17 +1,101 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SiteOverseer.Models
 {
     public class FacilityProgress
     {
         [Key]
+        [DisplayName("Progress ID")]
         public long ProgId { get; set; }
-        public required int  FlilTskId { get; set; }
-        public required DateOnly SubmitDte { get; set; }
-        public float? Longitude { get; set; }
-        public float? Latitude { get; set;}
-        public short CmpyId { get; set; }
-        public int UserId { get; set; }
-        public DateTime RevDteTime { get; set; }
+        [DisplayName("Facility Task ID")]
+        public required int  FcilTskId { get; set; }
+        [DisplayName("Progress Percent")]
+        public required short ProgPercent { get; set; }
+        [DisplayName("Submitted Date")]
+        public DateTime SubmitDte { get; set; }
+        public double? Longitude { get; set; }
+        public double? Latitude { get; set;}
+        [DisplayName("Company")]
+        public short? CmpyId { get; set; }
+        [DisplayName("User")]
+        public int? UserId { get; set; }
+        [DisplayName("Revised Datetime")]
+        public DateTime? RevDteTime { get; set; }
+        
+
+        //Custom
+        [NotMapped]
+        public int? FcilId { get; set; }
+        [NotMapped]
+        [DisplayName("Facility")]
+        public string? FcilNme { get; set; }
+        [NotMapped]
+        public string? FcilCde { get; set; }
+
+        [NotMapped]
+        public int? WbsdId { get; set; }
+        [NotMapped]
+        [DisplayName("WBS")]
+        public int? WbsId { get; set; }
+        [NotMapped]
+        public string? WbsCde { get; set; }
+        [NotMapped]
+        public string? WbsdCde { get; set; }
+
+        [NotMapped]
+        public int? CntorId { get; set; }
+        [NotMapped]
+        [DisplayName("Contractor")]
+        public string? CntorNme { get; set; }
+
+        [NotMapped]
+        [DisplayName("Work Start Date")]
+        public DateTime? WorkstartDte { get; set; }
+        [NotMapped]
+        [DisplayName("Work End Date")]
+        public DateTime? WorkendDte { get; set; }
+
+        [NotMapped]
+
+        [DisplayName("Task Complete")]
+        public  bool TaskCompleteFlg { get; set; }
+        [NotMapped]
+        public decimal? Budget { get; set; }
+        [DisplayName("Company")]
+        [NotMapped]
+        public string? Company { get; set; }
+        [DisplayName("User")]
+        [NotMapped]
+        public string? User { get; set; }
+        [NotMapped]
+        [DisplayName("Company")]
+        public string? FCompany { get; set; }
+        [DisplayName("User")]
+        [NotMapped]
+        public string? FUser { get; set; }
+        [NotMapped]
+        public short? FC_Id { get; set; }
+        [NotMapped]
+        public int? FU_Id { get; set; }
+        [NotMapped]
+        [DisplayName("Revised Datetime")]
+        public DateTime? RevdTetime { get; set; }
+        [NotMapped]
+        [DisplayName("Selection Type")]
+        public string? SelectionTyp { get; set; }
+        [NotMapped]
+        [DisplayName("Awarded Value")]
+        public decimal? AwardedValue { get; set; }
+        [NotMapped]
+        [DisplayName("Progress Payment")]
+        public int? ProgpayId { get; set; }
+        [NotMapped]
+        [DisplayName("Allow Submit Expense")]
+        public bool AllowSubmitExpense { get; set; }
+        [NotMapped]
+        public string? Remark { get; set; }
+
     }
 }
