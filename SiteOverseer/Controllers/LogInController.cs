d1a8f35792a1f4edf228a078da5cce7cf7564a7a
Nyi Lynn <54408043+liner774@users.noreply.github.com>
2024-08-05 15:39:39 +0630

    login controller code fix

--- SiteOverseer/Controllers/LogInController.cs
+++ SiteOverseer/Controllers/LogInController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
-using SiteOverseer.Common;
 using SiteOverseer.Common.EncryptDecryptService;
 using SiteOverseer.Data;
 using System.Collections.Generic;
@@ -24,60 +23,46 @@ namespace SiteOverseer.Controllers
             _encryptDecryptService = new EncryptDecryptService();
         }
 
-
-        #region // Main Methods //
-
-
         public IActionResult Index()
         {
             return View();
         }
 
-            
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Index(LogInUser user)
         {
-            if ((!string.IsNullOrEmpty(user.UserCde)) && (!string.IsNullOrEmpty(user.Pwd)))
+            if (!string.IsNullOrEmpty(user.UserCde) && !string.IsNullOrEmpty(user.Pwd))
             {
-                    
                 try
                 {
-                    var dbUser = _context.MS_User.FirstOrDefault(u => u.UserCde.ToLower() == user.UserCde.ToLower());
+                    var dbUser = await _context.MS_User
+                        .FirstOrDefaultAsync(u => u.UserCde.ToLower() == user.UserCde.ToLower());
 
-                    //if (dbUser != null)
                     if (dbUser != null && dbUser.Pwd != null)
                     {
-
                         string strbytes = Encoding.UTF8.GetString(dbUser.Pwd);
-
                         var decryptedPwd = _encryptDecryptService.DecryptString(strbytes);
 
-                        //if (user.Pwd != null)
                         if (user.Pwd == decryptedPwd)
                         {
                             try
                             {
-                                var claims = new List<Claim>() {
-                                new (ClaimTypes.NameIdentifier,user.UserCde)
-
+                                var claims = new List<Claim>
+                                {
+                                    new Claim(ClaimTypes.NameIdentifier, user.UserCde),
+                                    new Claim(ClaimTypes.Role, dbUser.Position) 
                                 };
 
                                 var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-                                var properties = new AuthenticationProperties()
+                                var properties = new AuthenticationProperties
                                 {
                                     AllowRefresh = true
                                 };
 
                                 await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), properties);
 
-                                var puser = await _context.MS_User
-                                .Where(x => x.UserCde == user.UserCde)
-                                .Select(x => x.UserCde)
-                                .FirstOrDefaultAsync() ?? "";
-
                                 return RedirectToAction("Index", "Home");
-
                             }
                             catch (Exception ex)
                             {
@@ -89,20 +74,17 @@ namespace SiteOverseer.Controllers
                         {
                             ViewBag.AlertMessage = "Authentication failed. Please check your credentials.";
                         }
-
                     }
                     else
                     {
                         ViewBag.AlertMessage = "Authentication failed. Please check your credentials.";
                     }
-
                 }
                 catch (Exception ex)
                 {
                     ViewBag.AlertMessage = ex.Message;
                     return View(user);
                 }
-
             }
             else
             {
@@ -110,11 +92,6 @@ namespace SiteOverseer.Controllers
             }
 
             return View(user);
-
         }
-
-        #endregion
-
-
     }
 }
